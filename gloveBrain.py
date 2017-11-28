48a8ff444b826b7667061908473c1635af168462
Matt Farejowicz <mfarejowicz@gmail.com>
2017-11-28 10:40:37 -0500

    reorganize

--- gloveBrain.py
+++ gloveBrain.py
@@ -1,22 +1,29 @@
 ## 6.01 Final Project
 ## Team KAM - Kevin Mao, Angel Alvarez, Matt Farejowicz
-## This will be the software that will run on the Pi to read analog voltages,
+## This will be the software that will run on the Glove Pi to read analog voltages,
 ## make robot motion decisions, and send to the robot.
-from soar.robot.pioneer import PioneerRobot
 
-robot = PioneerRobot()
+## Example analog read below!
 
-def on_load():
-    pass
+import spidev #import SPI library
 
-def on_start():
-    pass
+spi=spidev.SpiDev() #create an SPI device object
+spi.open(0,0) #tell it which select pin and SPI channel to use
+spi.max_speed_hz = 1953000 #Set SPI CLK frequency...don't change this...will only lead to heartache.
 
-def on_step(step_duration):
-    pass
+#simple function to read the value of a channel from the MCP3008 (channel is 0 to 7 inclusive as described in diagram above)
+#value comes back to you in volts from 0 to 3.3V
+def readValue(channel):
+    adc = spi.xfer2([1,(8+channel)<<4,0])
+    data = ((adc[1]&3)<<8)+adc[2]
+    return 3.3*data/1023 #scale 10 bit ADC to 0 to 3.3V reading
 
-def on_stop():
-    pass
+#need to measure a voltage greater than 3.3V?  Or less than 0V?  Too bad! You'll have to shift it...you can do that using op amps!
 
-def on_shutdown():
-    pass
+#simple use case
+
+import time
+
+while True:
+    print(readValue(5)) #read and print voltage on channel 5 of ADC (pin 6)
+    time.sleep(1)  #relax for 1 second before continuing
---
+++ robotBrain.py
@@ -0,0 +1,22 @@
+## 6.01 Final Project
+## Team KAM - Kevin Mao, Angel Alvarez, Matt Farejowicz
+## This will be the software that will run on the Robot to receive instructions
+## and feed them to the robot's motors.
+from soar.robot.pioneer import PioneerRobot
+
+robot = PioneerRobot()
+
+def on_load():
+    pass
+
+def on_start():
+    pass
+
+def on_step(step_duration):
+    pass
+
+def on_stop():
+    pass
+
+def on_shutdown():
+    pass
