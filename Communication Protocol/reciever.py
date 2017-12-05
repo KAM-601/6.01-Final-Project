163d780e9771da55f52cf37690d85ac6586e8691
AngelAlvie <alvareza@mit.edu>
2017-12-05 12:33:37 -0500

    "Added netowrking tests "

--- Communication Protocol/reciever.py
+++ Communication Protocol/reciever.py
@@ -1,27 +1,20 @@
-# This is reciever side code, It contains a number of functions that would be useful for the reciever (robot side)
-import sys
+# Echo client program
 import socket
 
-class Reciever:
-  #class variables
-      
-  def __init__(self, dataHandler):
-    self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # construct a socket 
-    self.handler = dataHandler
-    self.listening = True
+HOST = input('Please provide a host to connect to: ')
+PORT = 6010    # The same port as the server
 
-  def connect(self, HOST='localhost', PORT='6010'):
-    try:
-      self.Socket.connect((HOST, PORT))
-    except socket.error:
-      print('Failed to connect to Server')
-      return
-    try:
-      while self.listening:
-        self.Socket.sendall(b'Ready')
-        data = self.Socket.recv(1024) # stall until data recieved from the server
-        self.listening = self.handler(data) # set listening to the return value of the listener
-    except:
-      print('Server communication broken')
-    finally:
-      self.Socket.close()
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'ready')
+    counter = 100
+    while True:
+      data = s.recv(1024)
+      print('Received', repr(data))
+      counter += 1
+      if counter > 1000000:
+        s.sendall(b'stop')
+      else:
+        s.sendall(bytes(counter))
--- Communication Protocol/server.py
+++ Communication Protocol/server.py
@@ -1,29 +1,32 @@
 import socket
-import sys
+import time
+# Function to Figure out what IP address we are using,
+# using the following code snippet: https://stackoverflow.com/questions/24196932/how-can-i-get-the-ip-address-of-eth0-in-python
 
-class Server:
-  
-  def __init__(self, handler):
-    # create server socket
-    self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.handler = handler
-    self.connection = None
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))  # we don't actually care about connecting to the google DNS, instead we are using the
+    return s.getsockname()[0]   # garbage collector to remove our socket on return
 
-  def start(self,HOST='localhost', PORT = 6010):
-    self.Socket.bind((HOST, PORT))
-    self.Socket.listen(1)
-    self.connection, client_address = self.Socket.accept()
-    
-  def sendData(self, data):
-    try:
-      if self.connection:
-        self.connection.sendall(data)
-        return True
-      else:
-        return False
-    except:
-      print("could not send data to client")
 
-  def close(self):
-    self.connection.close()
-    self.Socket.close()
+HOSTNAME = get_ip_address()
+PORT = 6010
+
+
+# create the server socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOSTNAME, PORT))
+    s.listen(1)
+    print("Now Listening on: " + str(HOSTNAME) + ":" + str(PORT))
+    while True:
+      connection, addr = s.accept()
+      print("Connection to " + str(addr) + " initialized")
+      with connection:
+          while True:             # this would be the main loop of the glove brain
+              data = connection.recv(1024)
+              if data:
+                print(data)
+              time.sleep(1)
+              connection.sendall(b"more data")
+          connection.close()
+          print("Connection to " + str(addr) + " closed")
