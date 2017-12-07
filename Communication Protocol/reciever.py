c8f87bc13120c53d5bee73627917677368407e70
AngelAlvie <alvareza@mit.edu>
2017-12-07 11:12:18 -0500

    changed reciever code

--- Communication Protocol/reciever.py
+++ Communication Protocol/reciever.py
@@ -6,13 +6,23 @@ HOST = input('Please provide a host to connect to: ')
 PORT = 6010    # The same port as the server
 
 
-h1 = http.client.HTTPConnection('www.python.org')
+#h1 = http.client.HTTPConnection('www.python.org')
+
+
+def loop(data):
+  return None
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'ready')
-    while True:
-      data = s.recv(1024)
-      print(data.decode('UTF-8'))
-      sendData = input("Send data to server:").encode()
-      s.sendall(sendData)
+  s.connect((HOST, PORT))
+  s.sendall(b'ready')
+  while True:
+    data = s.recv(1024)
+    data = data.decode('UTF-8')
+
+    ### RUN THE MAIN LOOP OF THE RECIEVER
+    print(data)
+    sendData = loop(data)
+
+    if sendData != None:
+      s.sendall(sendData.encode('UTF-8'))
