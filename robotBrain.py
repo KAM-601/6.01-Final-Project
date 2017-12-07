82ff1e935c828353d1893342974a3c64ff7eaa25
Matt Farejowicz <mfarejowicz@gmail.com>
2017-12-07 11:25:13 -0500

    work on robot receiving

--- robotBrain.py
+++ robotBrain.py
@@ -5,20 +5,47 @@
 from soar.robot.pioneer import PioneerRobot
 import socket
 
-robot = PioneerRobot()
+HOST = input('Please provide a host to connect to: ')
+PORT = 6010    # The same port as the server
 
-def on_load():
-    
-    
+robot = PioneerRobot()
+robot.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+##def loop(data):
+##  return None
+##
+##  while True:
+##    data = s.recv(1024)
+##    data = data.decode('UTF-8')
+##
+##    ### RUN THE MAIN LOOP OF THE RECIEVER
+##    print(data)
+##    sendData = loop(data)
+##
+##    if sendData != None:
+##      s.sendall(sendData.encode('UTF-8'))
+
+
+def on_load():  
     pass
 
 def on_start():
-    pass
+    robot.s.connect((HOST, PORT))
+    robot.s.sendall(b'ready')
+  
 
 def on_step(step_duration):
+    #somewhere her I have to make sure dat is the latest data
+    data = s.recv(4096)
+    data = data.decode('UTF-8')
+    data.split(' ')
+    if len(data) != 4:
+        return 
     # Receives some instructions from gloveBrain.py
-    robot.fv = forward_velocity
-    robot.rv = rotational_velocity
+    if data[1] is not 'None':
+        robot.fv = float(data[1])
+    if data[3] is not 'None':
+        robot.rv = float(data[3])
 
 def on_stop():
     pass
