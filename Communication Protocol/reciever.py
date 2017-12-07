b138c7c9c573f5833311709e28ffdc207ab647cd
AngelAlvie <alvareza@mit.edu>
2017-12-07 10:29:30 -0500

    updated code

--- Communication Protocol/reciever.py
+++ Communication Protocol/reciever.py
@@ -1,20 +1,18 @@
 # Echo client program
 import socket
+import http.client
 
 HOST = input('Please provide a host to connect to: ')
 PORT = 6010    # The same port as the server
 
 
+h1 = http.client.HTTPConnection('www.python.org')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'ready')
-    counter = 100
     while True:
       data = s.recv(1024)
-      print('Received', repr(data))
-      counter += 1
-      if counter > 1000000:
-        s.sendall(b'stop')
-      else:
-        s.sendall(bytes(counter))
+      print(data.decode('UTF-8'))
+      sendData = input("Send data to server:").encode()
+      s.sendall(sendData)
--- Communication Protocol/server.py
+++ Communication Protocol/server.py
@@ -13,20 +13,22 @@ HOSTNAME = get_ip_address()
 PORT = 6010
 
 
+
 # create the server socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOSTNAME, PORT))
     s.listen(1)
     print("Now Listening on: " + str(HOSTNAME) + ":" + str(PORT))
     while True:
-      connection, addr = s.accept()
-      print("Connection to " + str(addr) + " initialized")
-      with connection:
-          while True:             # this would be the main loop of the glove brain
-              data = connection.recv(1024)
-              if data:
-                print(data)
-              time.sleep(1)
-              connection.sendall(b"more data")
-          connection.close()
-          print("Connection to " + str(addr) + " closed")
+        connection, addr = s.accept()
+        print("Connection to " + str(addr) + " initialized")
+        with connection:
+            while True:             # this would be the main loop of the glove brain
+                data = connection.recv(1024)
+                literal = data.decode('UTF-8')
+                if literal == 'stop':
+                    connection.close()
+                literal = literal.upper()
+                connection.sendall(literal.encode())
+        connection.close()
+    print("Connection to " + str(addr) + " closed")
